a7dff3a61417604d7d53ed1759b11fac4da53f30
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-10 10:15:20 -0500

    Tipos de Variables

---
+++ first_step/Test_variable.java
@@ -0,0 +1,12 @@
+package first_step;
+
+public class Test_variable {
+	public static void main(String [] args){
+
+        System.out.println("Hola de nuevo Leo");    
+
+        int edad = 30;
+
+        System.out.println("La edad de Leo es "+ edad + ", Felicitaciones!");
+    }
+}
---
+++ first_step/ejemplo.java
@@ -0,0 +1,8 @@
+package first_step;
+
+	public class ejemplo {
+		
+		public static void main(String[] args){
+			System.out.print("Hola Leo");
+	}
+}
