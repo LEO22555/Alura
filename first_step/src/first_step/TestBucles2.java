80e5735fbbbb1e8a7c3d00ff14c3222da39bf2d2
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-11 17:15:25 -0500

    Refuerzo

---
+++ first_step/src/first_step/TestBucles2.java
@@ -0,0 +1,17 @@
+package first_step;
+
+public class TestBucles2 {
+	    public static void main(String[] args) {
+
+	        for (int fila = 0; fila < 10; fila++) {
+	            for(int columna = 0; columna < 10; columna++) {
+	                if (columna > fila) {
+	                    break;
+	                }
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+	}
