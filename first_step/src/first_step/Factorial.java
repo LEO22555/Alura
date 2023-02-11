9d8b6b4e8457b67d868eb0d2462077fd4103bfb2
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-11 16:49:41 -0500

    Factorial

---
+++ first_step/src/first_step/Factorial.java
@@ -0,0 +1,14 @@
+package first_step;
+
+public class Factorial {
+
+	    public static void main(String[] args) {
+	        int factorial = 1;
+	        for (int i = 1; i < 11; i++) {
+	            factorial *= i;
+	            System.out.println("Factorial de " + i + " = " + factorial);
+	        }
+	    }
+
+	}
+
