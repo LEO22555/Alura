73dd6331ad01b52663052afb9c74036503465ab0
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-11 16:48:32 -0500

    Multiplos del 3

---
+++ first_step/src/first_step/MultiplosDeTresHastaCien.java
@@ -0,0 +1,13 @@
+package first_step;
+
+class MultiplosDeTresHastaCien {
+
+    public static void main (String[] args) {
+        for (int i = 1; i < 100; i++ ){
+            if (i % 3 == 0)    {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
