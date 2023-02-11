25395ad10e6d12e4e28a978c24abeca0f6da91b4
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-11 15:56:14 -0500

    ciclo for

---
+++ first_step/src/first_step/Ciclosfor.java
@@ -0,0 +1,10 @@
+package first_step;
+
+public class Ciclosfor {
+	public static void main(String[] args) {
+		//  variable          condicion      ejecutar al FINAL
+		for(int contador = 0; contador <=10; contador++) {
+			System.out.println(contador);
+		}
+}
+}
