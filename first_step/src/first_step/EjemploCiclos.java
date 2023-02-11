287e68b9176ff1986c391d588de5eb81c38d990d
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-11 15:11:03 -0500

    Ejemplo while

---
+++ first_step/src/first_step/EjemploCiclos.java
@@ -0,0 +1,17 @@
+package first_step;
+
+public class EjemploCiclos {
+	public static void main(String[] args) {
+		int contador = -5;
+		
+		while(contador<=5) { //mientras que (condicion sea verdadero)
+			System.out.println(contador);
+			//contador = contador + 1;
+			//contador +=1;
+			//contador ++;
+			++contador;
+			
+		}
+	
+}
+}
