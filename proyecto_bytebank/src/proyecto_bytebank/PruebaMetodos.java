a4f8a85d644fc266fd59d203c1fcb0ff04ec3d82
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-02-15 18:51:47 -0500

    metodo transferencia

--- proyecto_bytebank/src/proyecto_bytebank/PruebaMetodos.java
+++ proyecto_bytebank/src/proyecto_bytebank/PruebaMetodos.java
@@ -7,19 +7,19 @@ public class PruebaMetodos {
 		//metodo depositar
 		miCuenta.depositar(200);
 		System.out.println(miCuenta.saldo);
+		
 		//metodo retirar
 		miCuenta.retirar(100);
-		System.out.println(miCuenta.saldo);
-		if (cuentaDeJimena.transferir(400, miCuenta)) {
-			System.out.println("Transferencia exitosa");
-		}
-		
+		System.out.println(miCuenta.saldo);	
 		
 		Cuenta cuentaDeJimena = new Cuenta();
-		//metodo transferir
 		cuentaDeJimena.depositar(1000);
-		cuentaDeJimena.transferir(400, miCuenta);
-		
+		boolean puedeTransferir = cuentaDeJimena.transferir(400, miCuenta);
+		if(puedeTransferir){
+			System.out.println("Transferencia exitosa");
+		} else {
+			System.out.println("No es posible");
+		}
 		System.out.println(cuentaDeJimena.saldo);
 		System.out.println(miCuenta.saldo);
 		
