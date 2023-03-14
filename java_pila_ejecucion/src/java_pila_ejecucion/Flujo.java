b2547d65636bf2582060f8fd8ea1abe06fee1027
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-03-14 16:10:16 -0500

    manejo de excepciones

--- java_pila_ejecucion/src/java_pila_ejecucion/Flujo.java
+++ java_pila_ejecucion/src/java_pila_ejecucion/Flujo.java
@@ -17,6 +17,26 @@ public class Flujo {
 		System.out.println("Inicio metodo2");
 		for(int i = 1; i<=5; i++) {
 			System.out.println(i);
+			
+			try {
+				if (i==3) {
+				int num = 0;
+				int resultado = i/num;
+				System.out.println(resultado);
+				}
+				
+				String test = null;
+				System.out.println(test.toString());
+				
+			} catch(ArithmeticException exception) { //Atrapa el error
+				System.out.println("Atrapo Arithmetic");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+				} catch(NullPointerException exception) { //Atrapa el error
+				System.out.println("Atrapo NullPointer");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+				}
 		}
 		System.out.println("Fin metodo 2");
 	}
